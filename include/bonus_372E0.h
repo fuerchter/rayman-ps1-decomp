75d97316c5a05f5dea7944e2cc81ffd368f48ab4
fuerchter <fuerchter@gmail.com>
2023-10-11 20:39:31 +0200

    DO_WIZARD

--- include/bonus_372E0.h
+++ include/bonus_372E0.h
@@ -4,5 +4,6 @@
 #include "common.h"
 #include "obj_update.h"
 #include "world_map_95CC.h"
+#include "ray/ray_30A64.h"
 
 #endif
--- src/bonus_372E0.c
+++ src/bonus_372E0.c
@@ -1,6 +1,55 @@
 #include "bonus_372E0.h"
 
-INCLUDE_ASM("asm/nonmatchings/bonus_372E0", DO_WIZARD);
+/* 372E0 8015BAE0 -O2 */
+/*? fix_numlevel(u8 *);
+? save_objects_flags();
+? set_main_and_sub_etat(s32 *, ?, ?);
+? set_sub_etat(u8 *, ?);*/
+
+void DO_WIZARD(Obj *obj)
+{
+  s16 one;
+  
+  switch (obj->sub_etat)
+  {
+    case 1:
+        if (status_bar.num_wiz >= 10)
+        {
+            obj->field24_0x3e = status_bar.num_wiz;
+            set_sub_etat(obj, 2);
+            set_main_and_sub_etat(&ray, 3, 9);
+            return;
+        }
+        set_sub_etat(obj, 0xA);
+        obj->flags &= ~OBJ_FLIP_X;
+        return;
+
+    case 3:
+        set_main_and_sub_etat(&ray, 3, 0xA);
+        if (horloge[2] == 0 && (obj->field24_0x3e - status_bar.num_wiz < 10))
+        {
+            one = 1;
+            status_bar.num_wiz -= one;
+            return;
+        }
+        return;
+
+    case 5:
+        if (ray.main_etat == 3 && (ray.sub_etat - 0xB >= 2U))
+            set_sub_etat(&ray, 0xB);
+
+        if (obj->anim_frame >= obj->animations[obj->anim_index].frames_count - 1)
+        {
+            set_sub_etat(obj, 0);
+            fix_numlevel(obj);
+            save_objects_flags();
+
+            ray.flags |= OBJ_FLAG_0;
+            set_main_and_sub_etat((&ray), 0, 0);
+        }
+        break;
+  }
+}
 
 INCLUDE_ASM("asm/nonmatchings/bonus_372E0", get_next_bonus_level);
 
