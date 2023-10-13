081148e36c5d590e9ebb6e78986d19cdd1b71f5c
fuerchter <fuerchter@gmail.com>
2023-10-13 19:05:50 +0200

    caisse_claire

--- src/obj/caisse_claire_command.c
+++ src/obj/caisse_claire_command.c
@@ -1,4 +1,31 @@
 #include "obj/caisse_claire_command.h"
 
 /* 4D6EC 80171EEC -O2 */
-INCLUDE_ASM("asm/nonmatchings/obj/caisse_claire_command", DO_CCL_COMMAND);
+void DO_CCL_COMMAND(Obj *obj)
+{
+  u8 hp;
+
+  switch(obj->cmd)
+  {
+    case GO_WAIT:
+    case GO_NOP:
+        SET_X_SPEED(obj);
+        CALC_MOV_ON_BLOC(obj);
+        if (obj->main_etat == 0 && obj->sub_etat == 2)
+        {
+            hp = obj->hit_points;
+            if (hp != (obj->flags >> 0xe & 1))
+                obj->flags = obj->flags & ~OBJ_FLIP_X | (hp & 1) << 0xe;
+        }
+        break;
+    case GO_LEFT:
+    case GO_RIGHT:
+        if (obj->cmd == GO_LEFT)
+            obj->flags &= ~OBJ_FLIP_X;
+        else
+            obj->flags |= OBJ_FLIP_X;
+        SET_X_SPEED(obj);
+        CALC_MOV_ON_BLOC(obj);
+        break;
+  }
+}
