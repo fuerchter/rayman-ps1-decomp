bae1e844d1f2b63e4ee491b9cc9807d90c57dd8f
fuerchter <fuerchter@gmail.com>
2023-11-14 21:03:11 +0100

    mama_pirate .data

---
+++ src/data/obj/mama_pirate.c
@@ -0,0 +1,11 @@
+#include "obj/mama_pirate.h"
+
+u8 pma_couteaux[] = {3, 5, 5, 3, 3, 4};
+u8 pma_sequence[4][2] =
+{
+    {0, 3},
+    {5, 0},
+    {5, 1},
+    {4, 2}
+};
+u8 place_sequence[] = {3, 4, 2, 3, 2};
