5605d1d452020b2bc3773d7d2d125688c722d45c
fuerchter <fuerchter@gmail.com>
2023-12-02 19:37:28 +0100

    more .data

---
+++ src/data/loading_AA8C8.c
@@ -0,0 +1,11 @@
+#include "loading_794DC.h"
+
+u8 D_801CF0C8[2] = {};
+
+/* both of these are missing from linker map??? */
+u8 D_801CF0CA = 0;
+u8 PS1_HasLoadedFont = false;
+
+u8 D_801CF0CC = 0;
+u8 D_801CF0CD = 0;
+u8 s_filefxs_801cf0d0[] = "filefxs";
