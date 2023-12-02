eef3d73443391da8e3662cbce5479c848da3f58c
fuerchter <fuerchter@gmail.com>
2023-12-02 17:10:59 +0100

    more sound .data

---
+++ src/data/sound_AA7C0.c
@@ -0,0 +1,4 @@
+#include "sound.h"
+
+s16 PS1_SoundVolume = 127;
+u8 D_801CEFC4[] = { 0, 0, 1, 0 }; /* unknown raw bytes */
---
+++ src/data/sound_AA7CC.c
@@ -0,0 +1,8 @@
+#include "sound.h"
+
+s16 D_801CEFCC = 0;
+s16 D_801CEFCE = -2;
+u8 D_801CEFD0[] = { 50, 52, 54, 55, 57, 59, 61, 62 };
+s16 D_801CEFD8 = 0;
+s16 PS1_CurrentAudio = 25;
+s16 D_801CEFDC = 0;
