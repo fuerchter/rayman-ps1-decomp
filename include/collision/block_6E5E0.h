550a8b57d14eb0eb774c4cd78d7c9bf34aac5ee5
fuerchter <fuerchter@gmail.com>
2024-04-01 10:24:13 +0200

    block_6E5E0.c up to calc_typ_travd

--- include/collision/block_6E5E0.h
+++ include/collision/block_6E5E0.h
@@ -4,8 +4,10 @@
 #include "common.h"
 #include "common/ray.h"
 #include "obj_update.h"
+#include "obj_init_kill.h"
 #include "collision/etat.h"
 #include "collision/bit_util.h"
+#include "collision/block_22C84.h"
 
 u8 MURDUR(s16 x, s16 y);
 s16 dist_to_bloc_floor(s16 btyp, s16 x, s16 y);
--- src/collision/block_6E5E0.c
+++ src/collision/block_6E5E0.c
@@ -64,7 +64,76 @@ s16 bloc_floor(s16 btyp, s16 x, s16 y)
 
 INCLUDE_ASM("asm/nonmatchings/collision/block_6E5E0", calc_typ_trav);
 
+/* still pretty unreadable... */
+/* 6EA38 80193238 -O2 -msoft-float */
+#ifndef NONMATCHINGS /* missing_addiu */
 INCLUDE_ASM("asm/nonmatchings/collision/block_6E5E0", calc_typ_travd);
+#else
+u8 calc_typ_travd(Obj *obj, u8 param_2)
+{
+    s16 spd_x_sgn = obj->speed_x; /* ??? */
+    s16 x_offs_1;
+    s32 unk_1;
+    s16 x_saved;
+    s16 x_offs_2;
+    s16 foll_x; s16 foll_y; s16 foll_w; s16 foll_h;
+    u8 unk_2;
+    s32 res;
+
+    /* sgn() on android */
+    if (obj->speed_x >= 0)
+        spd_x_sgn = obj->speed_x > 0;
+    else
+        spd_x_sgn = -1;
+
+    if (spd_x_sgn != 0)
+    {
+        x_offs_1 = 14;
+        if (obj->type == TYPE_RAYMAN)
+        {
+            unk_1 = 4;
+            if (ray.scale != 0)
+            {
+                x_offs_1 = 8;
+                unk_1 = 2;
+            }
+
+            if (ray.field23_0x3c != -1)
+                x_offs_1 -= unk_1;
+        }
+        x_offs_1 *= spd_x_sgn;
+        if (param_2)
+            x_offs_1 = -x_offs_1;
+        x_saved = obj->x_pos;
+        obj->x_pos += x_offs_1;
+        if (!param_2)
+        {
+            x_offs_2 = obj->speed_x;
+            if (flags[obj->type].flags1 >> OBJ1_USE_INSTANT_SPEED_X & 1)
+                x_offs_2 /= 16;
+            obj->x_pos -= x_offs_2;
+        }
+
+        if (obj->flags & FLG(OBJ_FOLLOW_ENABLED))
+            GET_SPRITE_POS(obj, obj->follow_sprite, &foll_x, &foll_y, &foll_w, &foll_h);
+        else
+            foll_y = 0;
+        
+        unk_2 = ashr16(- (foll_y + obj->offset_hy) + obj->offset_by, 4);
+        if (obj->type == TYPE_RAYMAN && ray.eta[ray.main_etat][ray.sub_etat].flags & 0x40)
+            unk_2 = ashr16(unk_2, 1);
+        if (unk_2 != 0)
+            unk_2--;
+        res = calc_typ_trav(obj, unk_2);
+        obj->x_pos = x_saved;
+    }
+    else
+        res = 0;
+
+    __asm__("nop");
+    return res;
+}
+#endif
 
 INCLUDE_ASM("asm/nonmatchings/collision/block_6E5E0", TEST_FIN_BLOC);
 
