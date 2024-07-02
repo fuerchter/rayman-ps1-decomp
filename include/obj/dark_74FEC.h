0cb001798be54b76b64f3bd120aa8277ad5143ce
fuerchter <fuerchter@gmail.com>
2024-07-02 20:36:40 +0200

    dark_74FEC.c up to allocateMOSKITOSAXO

---
+++ include/obj/dark_74FEC.h
@@ -0,0 +1,13 @@
+#ifndef DARK_74FEC_H
+#define DARK_74FEC_H
+
+#include "common.h"
+#include "common/rayevts.h"
+#include "common/world_map.h"
+#include "obj/dark_729F0.h"
+
+void allocateSTOSKO(void);
+void allocateMOSKITOMAMA(void);
+void allocateMOSKITOSAXO(void);
+
+#endif
---
+++ src/obj/dark_74FEC.c
@@ -0,0 +1,84 @@
+#include "obj/dark_74FEC.h"
+
+extern u8 BBF2DEsk;
+extern u8 BBF2GEsk;
+extern s16 NiveauSol;
+extern s16 YPosBBF2D;
+extern s16 YPosBBF2G;
+
+/* 74FEC 801997EC -O2 -msoft-float */
+void allocateSTOSKO(void)
+{
+    s16 y;
+    Obj *obj = &level.objects[stosko_obj_id];
+
+    obj->flags |= FLG(OBJ_ALIVE)|FLG(OBJ_ACTIVE);
+    obj->x_pos = 160;
+    obj->y_pos = ymap - 50;
+    y = firstFloorBelow(obj) - obj->offset_by;
+    obj->init_y_pos = y;
+    obj->y_pos = y;
+    obj->x_pos = xmapmax + 320;
+    obj->init_x_pos = xmap - obj->offset_bx + 260;
+    obj->timer = 50;
+    obj->flags &= ~FLG(OBJ_FLIP_X);
+    RayEvts.flags0 |= FLG(RAYEVTS0_POING);
+    level.objects[poing_obj_id].init_sub_etat = 8;
+}
+
+/* 750E0 801998E0 -O2 -msoft-float */
+void allocateMOSKITOMAMA(void)
+{
+    s16 unk_1;
+    Obj *boss_obj;
+
+    unk_1 = 90;
+    YPosBBF2D =
+        (NiveauSol - (ray.offset_by - ray.offset_hy)) -
+        (((ray.x_pos + ray.offset_bx - xmap) >> 1) - unk_1);
+    boss_obj = &level.objects[moskitomama_droite_obj_id];
+    boss_obj->flags &= ~FLG(OBJ_FLIP_X);
+    boss_obj->init_x_pos = boss_obj->x_pos = 160;
+    boss_obj->init_y_pos = boss_obj->y_pos = ymap;
+    NiveauSol = firstFloorBelow(boss_obj);
+    boss_obj->speed_x = 0;
+    boss_obj->speed_y = 0;
+    boss_obj->init_x_pos = boss_obj->x_pos = 280 - boss_obj->offset_bx;
+    boss_obj->init_y_pos = boss_obj->y_pos = ymap - 200;
+    boss_obj->flags |= FLG(OBJ_ALIVE)|FLG(OBJ_ACTIVE);
+    BBF2DEsk = 0;
+
+    unk_1 = 70;
+    YPosBBF2G =
+        (NiveauSol - (ray.offset_by - ray.offset_hy)) +
+        (((ray.x_pos + ray.offset_bx - xmap) >> 1) - unk_1);
+    boss_obj = &level.objects[moskitomama_gauche_obj_id];
+    boss_obj->flags |= FLG(OBJ_FLIP_X);
+    boss_obj->init_x_pos = boss_obj->x_pos = 40 - boss_obj->offset_bx;
+    boss_obj->init_y_pos = boss_obj->y_pos = ymap - 200;
+    boss_obj->speed_x = 0;
+    boss_obj->speed_y = 0;
+    boss_obj->flags |= FLG(OBJ_ALIVE)|FLG(OBJ_ACTIVE);
+    BBF2GEsk = 0;
+
+    RayEvts.flags0 |= FLG(RAYEVTS0_POING);
+    level.objects[poing_obj_id].init_sub_etat = 8;
+}
+
+/* 752EC 80199AEC -O2 -msoft-float */
+void allocateMOSKITOSAXO(void)
+{
+    Obj *obj = &level.objects[moskitosaxo_obj_id];
+
+    if (!(RayEvts.flags1 & FLG(RAYEVTS1_DEMI)))
+    {
+        DO_NOVA(&ray);
+        RAY_DEMIRAY();
+    }
+    RayEvts.flags1 |= FLG(RAYEVTS1_RUN);
+    obj->flags |= FLG(OBJ_ALIVE)|FLG(OBJ_ACTIVE);
+    obj->init_x_pos = obj->x_pos = xmap - (obj->offset_bx - 160);
+    obj->init_y_pos = obj->y_pos = ymap - obj->offset_by;
+    RayEvts.flags0 |= 1;
+    level.objects[poing_obj_id].init_sub_etat = 8;
+}
