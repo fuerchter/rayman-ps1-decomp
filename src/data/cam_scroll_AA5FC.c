96a982a8efabc0c9679357e5396f14c855b48954
fuerchter <fuerchter@gmail.com>
2024-08-16 17:14:33 +0200

    src/data/music_AA6A4.c

--- src/data/cam_scroll_AA5FC.c
+++ src/data/cam_scroll_AA5FC.c
@@ -2,7 +2,7 @@
 
 /* this might work correctly once every function that uses them is added? */
 u8 D_801CEDFC = 0;
-u8 data_cam_scroll_AA5FC[2] = {};
+u8 data_cam_scroll_AA5FC[2] = {}; /* TODO: name this by 80... address instead */
 s16 dhspeed = 0;
 s16 dvspeed = 0;
 u8 joy_done = 0;
---
+++ src/data/music_AA6A4.c
@@ -0,0 +1,20 @@
+#include "music.h"
+
+s16 D_801CEEA4 = 0;
+s16 D_801CEEA6 = 0;
+s16 D_801CEEA8 = 0;
+s16 PS1_Mark_counter = 0;
+s16 data_music_801ceeac = -1;
+s16 data_music_801ceeae = -1;
+s16 PS1_Music_access_num = -1;
+s16 data_music_801ceeb2 = -1;
+s16 data_music_801ceeb4 = 1;
+u8 PS1_Music_pcom = 0;
+s16 D_801CEEB8 = 0;
+s16 D_801CEEBA = 0;
+s16 D_801CEEBC = 1;
+u8 s_s_801ceec0[] = "%s\n";
+u8 s_idle_801ceec4[] = "idle";
+u8 s_busy_801ceecc[] = "busy";
+u8 s_yes_801ceed4[] = "yes";
+u8 s_no_801ceed8[] = "no";
