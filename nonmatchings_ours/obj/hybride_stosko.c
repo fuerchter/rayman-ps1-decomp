57fd9ad70480c264f4eae9ffeebce35ab502be16
fuerchter <fuerchter@gmail.com>
2024-09-07 14:16:55 +0200

    attempt functions not yet in src/

--- nonmatchings_ours/obj/hybride_stosko.c
+++ nonmatchings_ours/obj/hybride_stosko.c
@@ -1,6 +1,9 @@
 #include "obj/hybride_stosko.h"
 
-/* gotos-only matches */
+/*
+gotos-only matches
+cannot figure out the case(s) that just set obj->timer = 0x32 and return
+*/
 /*INCLUDE_ASM("asm/nonmatchings/obj/hybride_stosko", doSTOSKOcommand);*/
 
 void doSTOSKOcommand(Obj *obj)
@@ -43,7 +46,7 @@ void doSTOSKOcommand(Obj *obj)
         {
             if (obj->timer != 0)
             {
-                obj->timer -= 1;
+                obj->timer--;
                 return;
             }
             set_sub_etat(obj, 1U);
@@ -118,15 +121,14 @@ void doSTOSKOcommand(Obj *obj)
 void doSTOSKOcommand(Obj *obj)
 {
     Obj *temp_a2_2;
-    s32 var_v0;
+    s16 var_v0;
     s32 temp_v1;
     u8 temp_a1;
     u8 temp_a2;
+    u8 flag_set;
 
     scrollLocked = 1;
-    temp_a1 = obj->main_etat;
-    temp_a2 = obj->sub_etat;
-    temp_v1 = (temp_a1 << 8) + temp_a2;
+    temp_v1 = (obj->main_etat << 8) + obj->sub_etat;
     if (temp_v1 == 6)
     {
         goto block_52;
@@ -193,26 +195,20 @@ block_15:
     {
         goto block_55;
     }
-    if (obj->x_pos > obj->init_x_pos)
+    if (obj->x_pos <= obj->init_x_pos)
     {
-        goto block_20;
+        set_main_and_sub_etat(obj, 0U, 0U);
+        set_main_and_sub_etat(&ray, 0U, 0U);
+        remoteRayXToReach = -0x7D00;
     }
-    set_main_and_sub_etat(obj, 0U, 0U);
-    set_main_and_sub_etat(&ray, 0U, 0U);
-    remoteRayXToReach = -0x7D00;
-    goto block_23;
-block_20:
-    remoteRayXToReach = (s16) (u16) scroll_start_x;
-    if (ray.main_etat != 3)
+    else
     {
-        goto block_23;
+        remoteRayXToReach = (s16) (u16) scroll_start_x;
+        if ((ray.main_etat == 3) && (ray.sub_etat == 0x14))
+        {
+            ray.flags |= 0x4000;
+        }
     }
-    if (ray.sub_etat != 0x14)
-    {
-        goto block_23;
-    }
-    ray.flags |= 0x4000;
-block_23:
     SET_X_SPEED(obj);
     return;
 block_24:
@@ -220,107 +216,73 @@ block_24:
     obj->speed_y = -8;
     return;
 block_25:
-    if (poing.is_active != 0)
+    if ((poing.is_active == 0) && (ray.main_etat != 2))
     {
-        goto block_36;
+        if (obj->timer != 0)
+        {
+            obj->timer--;
+            return;
+        }
+        set_sub_etat(obj, 1U);
+        return;
     }
-    if (ray.main_etat == 2)
-    {
-        goto block_36;
-    }
-    if (obj->timer == 0)
-    {
-        goto block_29;
-    }
-    obj->timer -= 1;
-    return;
-block_29:
-    set_sub_etat(obj, 1U);
-    return;
-block_30:
-    if (poing.is_active == 0)
-    {
-        goto block_38;
-    }
-    if (
-        __builtin_abs(
-            (s16) (((ray.offset_by + (u16) ray.y_pos) - (u16) obj->y_pos) - obj->offset_by)
-        ) < 0x30
-    )
-    {
-        goto block_35;
-    }
-    if ((u8) obj->anim_frame < 0xAU)
-    {
-        goto block_37;
-    }
-block_35:
-    set_sub_etat(obj, 0x0AU);
-    obj->anim_index = obj->eta[0][10].anim_index;
-    return;
-block_36:
-block_37:
     set_sub_etat(obj, 5U);
     return;
-block_38:
-    if (!(obj->eta[temp_a1][temp_a2].flags & 0x10))
+block_30:
+    if (poing.is_active != 0)
     {
-        goto block_41;
+        var_v0 = ((ray.offset_by + ray.y_pos) - (u16) obj->y_pos) - obj->offset_by;
+        if ((__builtin_abs(var_v0) < 0x30) || ((u8) obj->anim_frame >= 0xAU))
+        {
+            set_sub_etat(obj, 0x0AU);
+            obj->anim_index = obj->eta[0][10].anim_index;
+            return;
+        }
+        set_sub_etat(obj, 5U);
+        return;
     }
-    if (obj->anim_frame == 0)
+
+    flag_set = obj->eta[obj->main_etat][obj->sub_etat].flags & 0x10;
+    if(
+        ((flag_set && obj->anim_frame == 0) ||
+        (!flag_set && obj->anim_frame == obj->animations[obj->anim_index].frames_count - 1)) &&
+        horloge[obj->eta[obj->main_etat][obj->sub_etat].anim_speed & 0xf] == 0
+    )
     {
-        goto block_42;
+            allocateStoskoClaw(obj);
+            return;
     }
-    return;
-block_41:
-    if (obj->anim_frame != (obj->animations[obj->anim_index].frames_count - 1))
-    {
-        goto block_55;
-    }
-block_42:
-    if (horloge[obj->eta[obj->main_etat][obj->sub_etat].anim_speed & 0xF] != 0)
-    {
-        goto block_55;
-    }
-    allocateStoskoClaw(obj);
     return;
 block_44:
     obj->timer = 0x32;
 block_45:
     temp_a2_2 = &level.objects[obj->field24_0x3e];
-    if (temp_a2_2->speed_x <= 0)
+    if ((temp_a2_2->speed_x > 0) && (temp_a2_2->x_pos >= obj->x_pos))
     {
-        goto block_55;
+        obj->field24_0x3e = -1;
+        temp_a2_2->flags &= ~0x400;
+        set_sub_etat(obj, 3U);
+        
+        return;
     }
-    if (temp_a2_2->x_pos < obj->x_pos)
-    {
-        goto block_55;
-    }
-    obj->field24_0x3e = -1;
-    temp_a2_2->flags &= ~0x400;
-    set_sub_etat(obj, 3U);
     return;
 block_48:
 block_49:
+    /* TODO: case??? it's not default */
     obj->timer = 0x32;
     return;
 block_50:
-    if (poing.is_active != 0)
+    if (poing.is_active == 0)
     {
-        goto block_55;
+        set_sub_etat(obj, 0x0CU);
+        return;
     }
-    set_sub_etat(obj, 0x0CU);
     return;
 block_52:
-    if (ray.main_etat == 2)
+    if ((ray.main_etat != 2) && (poing.is_active == 0))
     {
-        goto block_55;
+        set_sub_etat(obj, 7U);
     }
-    if (poing.is_active != 0)
-    {
-        goto block_55;
-    }
-    set_sub_etat(obj, 7U);
 block_55:
     return;
 }
