d85d5e181acf1815a951d3f43a3c89ea7b4b6bef
fuerchter <fuerchter@gmail.com>
2024-09-09 13:14:00 +0200

    moteur_init_end.c up to INIT_MOTEUR_LEVEL

--- src/moteur_init_end.c
+++ src/moteur_init_end.c
@@ -1,5 +1,8 @@
 #include "moteur_init_end.h"
 
+extern u8 D_801D7850;
+extern u8 D_801F75C0;
+
 void init_flocons(void);
 
 /* 347C4 80158FC4 -O2 -msoft-float */
@@ -104,11 +107,77 @@ void INIT_MOTEUR(u8 new_lvl)
     PS1_PhotographerCollision();
 }
 
-INCLUDE_ASM("asm/nonmatchings/moteur_init_end", INIT_MOTEUR_BEGIN);
+/* 34BC8 801593C8 -O2 -msoft-float */
+void INIT_MOTEUR_BEGIN(void)
+{
+    fptr_init();
+    PS1_InitAllowedTime();
+    init_bonus_perfect();
+    LOAD_CONFIG();
+    init_finBossLevel();
+    new_level = true;
+    new_world = true;
+    status_bar.num_lives = 3;
+    ray.hit_points = 2;
+    poing.sub_etat = 1;
+    departlevel = true;
+    nb_continue = 5;
+    gele = 0;
+    You_Win = false;
+    PS1_RandSum = 17171;
+    RandomIndex = 0;
+    set_proj_center(160, 170);
+    INIT_RAY_BEGIN();
+    scroll_x = -1;
+    scroll_y = -1;
+    special_ray_mov_win_x_left = 0;
+    special_ray_mov_win_x_right = 0;
+    D_801F75C0 = 0;
+    NumDemo = 0;
+    PS1_ShouldClearPassword = true;
+    D_801D7850 = true;
+    left_time = 0;
+    life_becoz_wiz = false;
+    RunTimeDemo = 1800;
+}
 
-INCLUDE_ASM("asm/nonmatchings/moteur_init_end", INIT_MOTEUR_WORLD);
+/* 34CE8 801594E8 -O2 -msoft-float */
+void INIT_MOTEUR_WORLD(void)
+{
+    new_world = false;
+    new_level = true;
+}
 
-INCLUDE_ASM("asm/nonmatchings/moteur_init_end", INIT_MOTEUR_LEVEL);
+/* 34D04 80159504 -O2 -msoft-float */
+void INIT_MOTEUR_LEVEL(s16 new_lvl)
+{
+    if (!fin_du_jeu)
+    {
+        bonus_map = (s16) get_allowed_time() != -2;
+        if (!bonus_map && departlevel)
+            initGameSave();
+        INIT_MOTEUR(new_lvl);
+        if (!bonus_map && departlevel)
+        {
+            saveGameState(null, &save1);
+            saveGameState(null, &save2);
+            life_becoz_wiz = false;
+        }
+
+        if (!departlevel)
+        {
+            departlevel = true;
+            restore_objects_flags();
+        }
+        else
+            nb_wiz_collected = 0;
+        
+        correct_gendoor_link();
+        new_level = false;
+        ray_mode = MODE_RAYMAN;
+        RAY_MODE_SPEED = 16;
+    }
+}
 
 INCLUDE_ASM("asm/nonmatchings/moteur_init_end", restore_gendoor_link);
 
