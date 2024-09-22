4870e141e613633bb1cd4f2aae77e1fea1641c48
fuerchter <fuerchter@gmail.com>
2024-09-22 20:14:21 +0200

    timers.c up to PS1_InitAllowedTime

--- src/timers.c
+++ src/timers.c
@@ -1,5 +1,7 @@
 #include "timers.h"
 
+extern s16 allowed_time[192];
+
 /* 293C4 8014DBC4 -O2 -msoft-float */
 void INIT_HORLOGES(void)
 {
@@ -23,7 +25,228 @@ void horloges(void)
     map_time++;
 }
 
-INCLUDE_ASM("asm/nonmatchings/timers", PS1_InitAllowedTime);
+/* 29488 8014DC88 -O2 -msoft-float */
+void PS1_InitAllowedTime(void)
+{
+    s16 cnt_1;
+    s16 cnt_2;
+    s16 *cur;
+    
+    for (cnt_1 = 1; cnt_1 <= (s16) LEN(allowed_time) >> 5; cnt_1++)
+    {
+        cnt_2 = (cnt_1 - 1);
+        cur = &allowed_time[(s16) (cnt_2 << 5)];
+        switch (cnt_2)
+        {
+        case 0:
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = 30;
+            *cur++ = 45;
+            *cur++ = 35;
+            *cur++ = 25;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            break;
+        case 1:
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = 40;
+            *cur++ = 25;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            break;
+        case 2:
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = 45;
+            *cur++ = 20;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            break;
+        case 3:
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = 35;
+            *cur++ = 50;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            break;
+        case 4:
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = 35;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            break;
+        case 5:
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            *cur++ = -2;
+            break;
+        }
+    }
+}
+
+
 
 INCLUDE_ASM("asm/nonmatchings/timers", get_allowed_time);
 
