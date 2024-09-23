0ba992bb64374de8ab2a5cc3e9ecebeaa51702a9
fuerchter <fuerchter@gmail.com>
2024-09-23 20:21:02 +0200

    vignet.c up to DISPLAY_GAME_VIGNET

--- src/vignet.c
+++ src/vignet.c
@@ -7,12 +7,63 @@ void DISPLAY_TXT_VIGNET(void)
 
     for (
         i = new_txt_fee;
-        text_to_display[i].text[0] != null && (i <= new_txt_fee + 1);
+        text_to_display[i].text[0] != '\0' && (i <= new_txt_fee + 1);
         i++
     )
         display_box_text(&text_to_display[i]);
 }
 
-INCLUDE_ASM("asm/nonmatchings/vignet", display_vignet_prg);
+/* 9AC4 8012E2C4 -O2 -msoft-float */
+s16 display_vignet_prg(void)
+{
+    s16 unk_1;
+    s16 unk_2;
 
-INCLUDE_ASM("asm/nonmatchings/vignet", DISPLAY_GAME_VIGNET);
+    readinput();
+    DO_FADE();
+    DISPLAY_FOND3();
+    DISPLAY_TXT_VIGNET();
+    if (++loop_nb_trames == 60)
+    {
+        loop_nb_trames = 0;
+        loop_timing--;
+    }
+
+    unk_1 = false;
+    if (
+        (
+            (ValidButPressed() || StartButPressed()) &&
+            loop_timing < 4
+        ) || loop_timing < 1
+    )
+        unk_1 = true;
+    
+    if (unk_1)
+    {
+        if (text_to_display[new_txt_fee + 2].text[0] == '\0')
+            new_txt_fee = 10;
+        else
+        {
+            unk_1 = false;
+            loop_timing = 5;
+            new_txt_fee += 2;
+        }
+    }
+
+    unk_2 = unk_1 && new_txt_fee == 10;
+    return unk_2;
+}
+
+/* 9C28 8012E428 -O2 -msoft-float */
+void DISPLAY_GAME_VIGNET(void)
+{
+    FUN_8016924c();
+    DO_FADE_OUT();
+    INIT_FADE_IN();
+    INIT_VIGNET();
+    SYNCHRO_LOOP(display_vignet_prg);
+    DO_FADE_OUT();
+    Vignet_To_Display = 0;
+    PROC_EXIT = false;
+    INIT_FADE_IN();
+}
