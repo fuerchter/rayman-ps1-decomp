64339ea9bd8272e4be34c2563011cc91b447ef44
fuerchter <fuerchter@gmail.com>
2024-09-23 21:18:39 +0200

    AACC.c up to PouvoirsParMap

---
+++ include/unknown/AACC.h
@@ -0,0 +1,12 @@
+#ifndef UNK_AACC_H
+#define UNK_AACC_H
+
+#include "common.h"
+#include "save.h"
+
+void FUN_8012f2cc(void);
+void PouvoirsParMap(void);
+void PS1_ResetSaveZone(void);
+void FUN_8012f570(void);
+
+#endif
---
+++ src/unknown/AACC.c
@@ -0,0 +1,15 @@
+#include "unknown/AACC.h"
+
+extern RaymanEvents D_801CEE84;
+extern RaymanEvents D_801CEE88;
+extern RaymanEvents D_801CEE8C;
+extern RaymanEvents D_801CEE90;
+extern RaymanEvents D_801CEE94;
+
+void FUN_8012f2cc(void) {}
+
+INCLUDE_ASM("asm/nonmatchings/unknown/AACC", PouvoirsParMap);
+
+INCLUDE_ASM("asm/nonmatchings/unknown/AACC", PS1_ResetSaveZone);
+
+INCLUDE_ASM("asm/nonmatchings/unknown/AACC", FUN_8012f570);
