20f63e86fdd07d2c4dac15d4a546ad92d14e7ff8
fuerchter <fuerchter@gmail.com>
2024-09-23 17:46:12 +0200

    fond_5BD8C.c up to FUN_8018058c

---
+++ src/fond_5BD8C.c
@@ -0,0 +1,13 @@
+#include "fond_5BD8C.h"
+
+INCLUDE_ASM("asm/nonmatchings/fond_5BD8C", FUN_8018058c);
+
+INCLUDE_ASM("asm/nonmatchings/fond_5BD8C", DISPLAY_FOND3);
+
+INCLUDE_ASM("asm/nonmatchings/fond_5BD8C", FUN_80180804);
+
+INCLUDE_ASM("asm/nonmatchings/fond_5BD8C", FUN_801809fc);
+
+INCLUDE_ASM("asm/nonmatchings/fond_5BD8C", DISPLAY_FOND_CONTINUE);
+
+INCLUDE_ASM("asm/nonmatchings/fond_5BD8C", DISPLAY_FOND_SELECT);
