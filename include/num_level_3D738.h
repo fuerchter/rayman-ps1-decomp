429d1917ce62b13c6221867d38adf70348026e4f
fuerchter <fuerchter@gmail.com>
2024-09-25 12:22:38 +0200

    ChangeLevel

---
+++ include/num_level_3D738.h
@@ -0,0 +1,9 @@
+#ifndef NUM_LEVEL_3D738_H
+#define NUM_LEVEL_3D738_H
+
+#include "common.h"
+#include "num_level_36260.h"
+
+void ChangeLevel(void);
+
+#endif
---
+++ src/num_level_3D738.c
@@ -0,0 +1,8 @@
+#include "num_level_3D738.h"
+
+/* 3D738 80161F38 -O2 -msoft-float */
+void ChangeLevel(void)
+{
+    fix_numlevel(&ray);
+    new_level = true;
+}
