639ee7cdfa8e54d79c9896957a8d79e808639053
fuerchter <fuerchter@gmail.com>
2024-09-25 12:57:10 +0200

    GetY

---
+++ include/unknown/3ED10.h
@@ -0,0 +1,16 @@
+#ifndef UNK_3ED10_H
+#define UNK_3ED10_H
+
+#include "common.h"
+#include "common/level.h"
+#include "obj/eau.h"
+
+/* .data */
+extern s16 EauDy[14];
+
+/* .data */
+extern s16 DecEau[4];
+
+s16 GetY(s16 x);
+
+#endif
---
+++ src/unknown/3ED10.c
@@ -0,0 +1,3 @@
+#include "unknown/3ED10.h"
+
+INCLUDE_ASM("asm/nonmatchings/unknown/3ED10", GetY);
