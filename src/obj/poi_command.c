d3fa082d0690276777ee89e61705c7f8e2a5bfec
fuerchter <fuerchter@gmail.com>
2024-09-28 19:57:27 +0200

    DO_POISSON_BLEU_CMD

--- src/obj/poi_command.c
+++ src/obj/poi_command.c
@@ -31,4 +31,69 @@ void DO_POISSON_VERT_CMD(Obj *obj)
     DO_OBJ_COLLISIONS(obj, 4);
 }
 
-INCLUDE_ASM("asm/nonmatchings/obj/poi_command", DO_POISSON_BLEU_CMD);
+/* 4F0CC 801738CC -O2 -msoft-float */
+void DO_POISSON_BLEU_CMD(Obj *obj)
+{
+    u8 flag_set;
+
+    DO_ONE_CMD(obj);
+    if (
+        (
+            !(obj->flags & FLG(OBJ_FLIP_X)) &&
+            on_block_chdir(obj, obj->offset_bx - 20, obj->offset_by - 3) &&
+            (s16) test_allowed(obj, obj->offset_bx - 20, obj->offset_by - 3)
+        ) ||
+        (
+            obj->flags & FLG(OBJ_FLIP_X) &&
+            on_block_chdir(obj, obj->offset_bx + 20, obj->offset_by - 3) &&
+            (s16) test_allowed(obj, obj->offset_bx + 20, obj->offset_by - 3)
+        )
+    )
+    {
+        if (obj->flags & FLG(OBJ_FLIP_X))
+        {
+            obj->flags &= ~FLG(OBJ_FLIP_X);
+            skipToLabel(obj, 13, true);
+        }
+        else
+        {
+            obj->flags |= FLG(OBJ_FLIP_X);
+            skipToLabel(obj, 14, true);
+        }
+        obj->y_pos = obj->init_y_pos;
+    }
+  
+    if (obj->main_etat == 2 && obj->sub_etat == 17)
+    {
+        if (obj->init_y_pos <= obj->y_pos)
+        {
+            if ((obj->flags & FLG(OBJ_FLIP_X)))
+                skipToLabel(obj, 10, true);
+            else
+                skipToLabel(obj, 9, true);
+            obj->y_pos = obj->init_y_pos;
+        }
+    }
+    if (obj->main_etat == 2 && obj->sub_etat == 18)
+    {
+        flag_set = obj->eta[obj->main_etat][obj->sub_etat].flags & 0x10;
+        if(
+            ((flag_set && obj->anim_frame == 0) ||
+            (!flag_set && obj->anim_frame == obj->animations[obj->anim_index].frames_count - 1)) &&
+            horloge[obj->eta[obj->main_etat][obj->sub_etat].anim_speed & 0xf] == 0
+        )
+        {
+            if (obj->speed_y == 5)
+            {
+                obj->display_prio = 0;
+                obj->flags &= ~FLG(OBJ_ACTIVE);
+            }
+            else
+            {
+                obj->speed_x = 0;
+                obj->speed_y = 5;
+                skipToLabel(obj, 12, true);
+            }
+        }
+    }
+}
