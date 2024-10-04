4fcadf47dafcd81903297a7a6ca8b956a6c1c41c
fuerchter <fuerchter@gmail.com>
2024-10-04 18:05:34 +0200

    ufo_idc.c attempts up to DO_MOVING_WITH_INDICATOR_COMMAND

--- include/obj/ufo_idc.h
+++ include/obj/ufo_idc.h
@@ -4,7 +4,7 @@
 #include "common.h"
 #include "obj_init_kill.h"
 
-u8 test_block_chdir(Obj *obj, s16 *param_2, s16 off_x, s16 off_y);
+s16 test_block_chdir(Obj *obj, s16 *out_unk, s16 off_x, s16 off_y);
 void DO_MOVING_WITH_INDICATOR_COMMAND(Obj *obj);
 void DO_IDC_COMMAND(Obj *obj);
 void DO_LEV_POING_COLLISION(Obj *obj);
--- src/obj/ufo_idc.c
+++ src/obj/ufo_idc.c
@@ -1,6 +1,24 @@
 #include "obj/ufo_idc.h"
 
-INCLUDE_ASM("asm/nonmatchings/obj/ufo_idc", test_block_chdir);
+/* 74954 80199154 -O2 -msoft-float */
+s16 test_block_chdir(Obj *obj, s16 *out_unk, s16 off_x, s16 off_y)
+{
+    u8 res = false;
+    s16 obj_x = (obj->x_pos + off_x) >> 4;
+    s16 obj_y = (obj->y_pos + off_y) >> 4;
+    
+    *out_unk = obj_x + obj_y * mp.width;
+    if (*out_unk != obj->test_block_index)
+    {
+        obj->test_block_index = *out_unk;
+        if (
+            obj_x >= 0 && (mp.width - 1 >= obj_x) &&
+            obj_y >= 0 && (mp.height - 1 >= obj_y)
+        )
+            res = (mp.map[*out_unk] & 0xFC00) == 0x400;
+    }
+    return res;
+}
 
 INCLUDE_ASM("asm/nonmatchings/obj/ufo_idc", DO_MOVING_WITH_INDICATOR_COMMAND);
 
