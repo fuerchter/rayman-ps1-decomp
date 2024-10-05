b78e25a19253e48fd51f9fc97e03f21c940668b3
fuerchter <fuerchter@gmail.com>
2024-10-05 11:37:37 +0200

    stonewoman.c up to DO_NOMOVE_STONEWOMAN_COMMAND

--- src/obj/stonewoman.c
+++ src/obj/stonewoman.c
@@ -10,4 +10,29 @@ u8 IS_STONEWOMAN_WAIT(Obj *obj)
 
 INCLUDE_ASM("asm/nonmatchings/obj/stonewoman", DO_STONEWOMAN_COMMAND);
 
-INCLUDE_ASM("asm/nonmatchings/obj/stonewoman", DO_NOMOVE_STONEWOMAN_COMMAND);
+/* 4ECB0 801734B0 -O2 -msoft-float */
+void DO_NOMOVE_STONEWOMAN_COMMAND(Obj *obj)
+{
+    s32 prev_flip_x;
+
+    if (IS_STONEWOMAN_WAIT(obj))
+    {
+        obj->speed_x = 0;
+        prev_flip_x = obj->flags >> OBJ_FLIP_X & 1;
+        calc_obj_dir(obj);
+        if ((obj->flags >> OBJ_FLIP_X & 1) != prev_flip_x)
+            skipToLabel(obj, 11, false);
+        
+        if (obj->timer == 0)
+            DO_STONEWOMAN_TIR(obj);
+        else
+            obj->timer--;
+    }
+    else
+    {
+        DO_STONEWOMAN_TIR(obj);
+        if (obj->main_etat == 0 && obj->sub_etat == 9)
+            obj->timer = 250;
+    }
+    CALC_MOV_ON_BLOC(obj);
+}
