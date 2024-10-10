bb77a06915543ab75e61a1cd1b36ae0d4cfe54ba
fuerchter <fuerchter@gmail.com>
2024-10-10 15:22:27 +0200

    data/draw/rollup_trans_AA5E4 data/unknown/AA608

---
+++ src/data/draw/rollup_trans_AA5E4.c
@@ -0,0 +1,9 @@
+#include "draw/rollup_trans.h"
+
+DVECTOR D_801CEDE4 = { .vx = 704, .vy = 0 };
+DVECTOR D_801CEDE8 = { .vx = SCREEN_WIDTH, .vy = SCREEN_HEIGHT };
+DVECTOR D_801CEDEC[2] =
+{
+    { .vx = 704, .vy = 0 },
+    { .vx = SCREEN_WIDTH, .vy = SCREEN_HEIGHT }
+}
---
+++ src/data/unknown/AA608.c
@@ -0,0 +1,3 @@
+#include "unknown/3ED10.h"
+
+s16 DecEau[] = { 9, 0, 7, 5 };
