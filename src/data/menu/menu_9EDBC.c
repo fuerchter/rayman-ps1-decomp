280b36bcdb29cea675fc8d983783a630d4092dec
fuerchter <fuerchter@gmail.com>
2024-10-10 11:51:58 +0200

    data/victoire_9EDF4

--- src/data/menu/menu_9EDBC.c
+++ src/data/menu/menu_9EDBC.c
@@ -6,7 +6,7 @@ u8 *PS1_SettingStrings[] =
     s_fist_801cee3c,
     s_action_801cee34,
     s_music_801cee2c,
-    null,
+    "sound fx",
     s_stereo_801cee24,
     s_mono_801cee1c,
     s_exit_801cee14
