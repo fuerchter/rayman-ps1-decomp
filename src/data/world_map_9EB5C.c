553572f55020fd09301b9223e46329c0eb2f0d8c
fuerchter <fuerchter@gmail.com>
2024-10-13 11:55:14 +0200

    make world_map_9EB5C build again

--- src/data/world_map_9EB5C.c
+++ src/data/world_map_9EB5C.c
@@ -1,33 +1,33 @@
 #include "world_map_677C0.h"
 
-/*WorldInfo t_world_info[24] =
+WorldInfo t_world_info[24] =
 {
-    { 29, 264, 18, 0, 0, 1, 1, 0, 1, 1, 0, "/pink plant woods/" },
-    { 100, 254, 2, 1, 0, 4, 0, 0, 1, 5, 12, "/anguish lagoon/" },
-    { 90, 212, 2, 1, 3, 2, 0, 0, 1, 9, 3, "/the swamps of forgetfulness/" },
-    { 47, 200, 3, 3, 3, 2, 0, 0, 1, 12, 2, "/ moskito's nest/" },
-    { 180, 215, 5, 19, 1, 4, 0, 0, 2, 1, 3, "/bongo hills/" },
-    { 185, 175, 8, 4, 6, 8, 0, 0, 2, 7, 10, "/allegro presto/" },
-    { 135, 160, 20, 6, 7, 5, 0, 0, 2, 12, 3, "/gong heights/" },
-    { 91, 143, 7, 7, 7, 6, 0, 0, 2, 14, 5, "/mr sax's hullaballo/" },
-    { 212, 133, 9, 5, 5, 8, 0, 0, 3, 1, 14, "/twilight gulch/" },
-    { 209, 92, 21, 8, 9, 10, 0, 0, 3, 3, 12, "/the hard rocks/" },
-    { 257, 80, 10, 11, 9, 10, 0, 0, 3, 6, 15, "/mr stone's peaks/" },
-    { 234, 235, 10, 11, 11, 12, 0, 0, 4, 1, 1, "/eraser plains/" },
-    { 278, 190, 13, 11, 11, 12, 0, 0, 4, 5, 9, "/pencil pentathlon/" },
-    { 290, 145, 22, 12, 13, 14, 0, 0, 4, 8, 15, "/space mama's crater/" },
-    { 335, 180, 13, 15, 13, 14, 0, 0, 5, 1, 10, "/crystal palace/" },
-    { 312, 228, 14, 23, 15, 16, 0, 0, 5, 3, 7, "/eat at joe's/" },
-    { 360, 220, 17, 16, 15, 16, 0, 0, 5, 9, 3, "/mr skops' stalactites/" },
-    { 357, 80, 17, 16, 17, 17, 0, 0, 6, 1, 0, "/mr dark's dare/" },
-    { 10, 212, 18, 0, 18, 18, 0, 0, 7, 20, 7, &D_801CEE10 },
-    { 182, 266, 4, 19, 19, 19, 0, 0, 7, 20, 4, &D_801CEE10 },
-    { 145, 97, 20, 6, 20, 20, 0, 0, 7, 20, 13, &D_801CEE10 },
-    { 190, 45, 21, 9, 21, 21, 0, 0, 7, 20, 0, &D_801CEE10 },
-    { 276, 115, 22, 13, 22, 22, 0, 0, 7, 20, 2, &D_801CEE10 },
-    { 306, 267, 15, 23, 23, 23, 0, 0, 7, 20, 1, &D_801CEE10 }
+    { 29, 264, 18, 0, 0, 1, true, false, false, 0, 1, 1, 0, "/pink plant woods/" },
+    { 100, 254, 2, 1, 0, 4, false, false, false, 0, 1, 5, 12, "/anguish lagoon/" },
+    { 90, 212, 2, 1, 3, 2, false, false, false, 0, 1, 9, 3, "/the swamps of forgetfulness/" },
+    { 47, 200, 3, 3, 3, 2, false, false, false, 0, 1, 12, 2, "/ moskito's nest/" },
+    { 180, 215, 5, 19, 1, 4, false, false, false, 0, 2, 1, 3, "/bongo hills/" },
+    { 185, 175, 8, 4, 6, 8, false, false, false, 0, 2, 7, 10, "/allegro presto/" },
+    { 135, 160, 20, 6, 7, 5, false, false, false, 0, 2, 12, 3, "/gong heights/" },
+    { 91, 143, 7, 7, 7, 6, false, false, false, 0, 2, 14, 5, "/mr sax's hullaballo/" },
+    { 212, 133, 9, 5, 5, 8, false, false, false, 0, 3, 1, 14, "/twilight gulch/" },
+    { 209, 92, 21, 8, 9, 10, false, false, false, 0, 3, 3, 12, "/the hard rocks/" },
+    { 257, 80, 10, 11, 9, 10, false, false, false, 0, 3, 6, 15, "/mr stone's peaks/" },
+    { 234, 235, 10, 11, 11, 12, false, false, false, 0, 4, 1, 1, "/eraser plains/" },
+    { 278, 190, 13, 11, 11, 12, false, false, false, 0, 4, 5, 9, "/pencil pentathlon/" },
+    { 290, 145, 22, 12, 13, 14, false, false, false, 0, 4, 8, 15, "/space mama's crater/" },
+    { 335, 180, 13, 15, 13, 14, false, false, false, 0, 5, 1, 10, "/crystal palace/" },
+    { 312, 228, 14, 23, 15, 16, false, false, false, 0, 5, 3, 7, "/eat at joe's/" },
+    { 360, 220, 17, 16, 15, 16, false, false, false, 0, 5, 9, 3, "/mr skops' stalactites/" },
+    { 357, 80, 17, 16, 17, 17, false, false, false, 0, 6, 1, 0, "/mr dark's dare/" },
+    { 10, 212, 18, 0, 18, 18, false, false, false, 0, 7, 20, 7, &D_801CEE10 },
+    { 182, 266, 4, 19, 19, 19, false, false, false, 0, 7, 20, 4, &D_801CEE10 },
+    { 145, 97, 20, 6, 20, 20, false, false, false, 0, 7, 20, 13, &D_801CEE10 },
+    { 190, 45, 21, 9, 21, 21, false, false, false, 0, 7, 20, 0, &D_801CEE10 },
+    { 276, 115, 22, 13, 22, 22, false, false, false, 0, 7, 20, 2, &D_801CEE10 },
+    { 306, 267, 15, 23, 23, 23, false, false, false, 0, 7, 20, 1, &D_801CEE10 }
 };
 
 u8 *PTR_s_save_game_801c353c = "/save game/";
 u8 *PTR_s_game_saved_801c3540 = "/game saved/";
-u8 *PTR_s_no_save_available_801c3544 = "/no save available/";*/
+u8 *PTR_s_no_save_available_801c3544 = "/no save available/";
