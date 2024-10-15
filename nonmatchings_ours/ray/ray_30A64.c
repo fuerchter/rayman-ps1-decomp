239d3e1be16020abfdd8e721c85de71d6f5fe7c5
fuerchter <fuerchter@gmail.com>
2024-10-15 16:37:10 +0200

    move_up_ray matches, but

--- nonmatchings_ours/ray/ray_30A64.c
+++ nonmatchings_ours/ray/ray_30A64.c
@@ -1,41 +1,29 @@
 #include "ray/ray_30A64.h"
 
-/*
-attempts: 2
-tried looking at android
-*/
+/* matches, but unk_y_2 */
 /*INCLUDE_ASM("asm/nonmatchings/ray/ray_30A64", move_up_ray);*/
 
 void move_up_ray(void)
 {
-    s32 temp_v1;
-    s32 var_a1;
-    s16 temp_v0;
-    s16 test_1;
+    s16 unk_y_1;
+    s16 unk_y_2;
 
-    if (ray_mode == 2 && RayCoince(2))
-    {
+    if (ray_mode == MODE_RAY_ON_MS && RayCoince(2))
         ray.speed_y = 0;
-    }
     
-    if (!(ray.eta[ray.main_etat][ray.sub_etat].flags & 0x40) && (ray.scale == 0))
-    {
-        var_a1 = 0x19;
-    }
+    if (!(ray.eta[ray.main_etat][ray.sub_etat].flags & 0x40) && ray.scale == 0)
+        unk_y_1 = 25;
     else
-        var_a1 = 0x28;
+        unk_y_1 = 40;
     
     ray.y_pos += ray.speed_y;
-    test_1 = scroll_start_y;
-    temp_v1 = var_a1 + 0x14;
-    if ((s16) ray.y_pos < (test_1 - temp_v1))
+    unk_y_2 = 20;
+    if (ray.y_pos < scroll_start_y - (unk_y_1 + unk_y_2))
     {
-        ray.y_pos = test_1 - temp_v1;
+        ray.y_pos = scroll_start_y - (unk_y_1 + unk_y_2);
         ray.speed_y = 0;
         if (ray.field20_0x36 != -1)
-        {
             RAY_HIT(ray.iframes_timer == -1, null);
-        }
     }
     calc_obj_pos(&ray);
     v_scroll_speed = 0;
