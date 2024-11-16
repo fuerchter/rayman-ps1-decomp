1b5e7488bd80a2fd9f9071fb6a9bb953fffcc7d4
fuerchter <fuerchter@gmail.com>
2024-11-16 10:21:56 +0100

    fix_numlevel

--- src/num_level_36260.c
+++ src/num_level_36260.c
@@ -16,234 +16,150 @@ void new_level_init(void)
     ymapsave = ymap;
 }
 
-#ifndef MATCHES_BUT
-INCLUDE_ASM("asm/nonmatchings/num_level_36260", fix_numlevel);
-#else
-/* matches, but clean up */
-/*INCLUDE_ASM("asm/nonmatchings/num_level_36260", fix_numlevel);*/
-
+/* 362B4 8015AAB4 -O2 -msoft-float */
 void fix_numlevel(Obj *obj)
 {
-    s16 temp_v0_1;
-    s16 temp_v0_2;
-    s16 temp_v0_3;
-    s16 temp_v0_4;
-    s16 temp_v0_5;
-    s16 temp_v0_6;
-    s16 var_v0_2;
-    s32 *var_a0;
-    s32 temp_v1;
-    s16 var_s0;
-    s32 var_v0;
+    s16 unk_1 = false;
 
-    var_s0 = 0;
-    if (departlevel != 0)
+    if (departlevel && (new_level_init(), obj->type != TYPE_WIZARD1))
     {
-        new_level_init();
-        if (obj->type != 5)
+        switch (num_world)
         {
-            switch (num_world)
-            {
-            case 1:
-                if ((num_level == 2) && (RayEvts.poing))
-                {
-                    num_level_choice = (u16) num_level_choice + 1;
-                }
-                if ((num_level == 5) && finBosslevel.bzzit)
-                {
-                    num_level_choice = (u16) num_level_choice + 1;
-                }
-                if (num_level == 7)
-                {
-                    if (RayEvts.hang)
-                    {
-                        new_world = 1;
-                    }
-                }
-                if ((num_level == 0x000D) && finBosslevel.moskito)
-                {
-                    num_level_choice = (u16) num_level_choice + 1;
-                }
-                if (num_level == 0x000F)
-                {
-                    if (finBosslevel.moskito)
-                    {
-                        new_world = 1;
-                    }
-                }
-                if (num_level == 0x0010)
-                {
-                    if (RayEvts.grap)
-                    {
-                        new_world = 1;
-                    }
-                }
-                break;
-            case 2:
-                if (num_level == 5)
-                {
-                    if (finBosslevel.crazy_drummer)
-                    {
-                        new_world = 1;
-                    }
-                }
-                if ((num_level == 9) && finBosslevel.mr_sax)
-                {
-                    new_world = 1;
-                }
-                if (num_level == 0x000A)
-                {
-                    if (RayEvts.helico)
-                    {
-                        new_world = 1;
-                    }
-                }
-                if (num_level == 0x000E)
-                {
-                    if (finBosslevel.mr_sax != 0)
-                    {
-                        new_world = 1;
-                    }
-                }
-                break;
-            case 3:
-                if (num_level == 1)
-                {
-                    if (finBosslevel.mr_stone)
-                    {
-                        new_world = 1;
-                    }
-                }
-                if ((num_level == 9) && (finBosslevel.mr_stone))
-                {
-                    new_world = 1;
-                }
-                if (num_level == 0x000A)
-                {
-                    var_v0 = RayEvts.run;
-                    if (var_v0 != 0)
-                    {
-                        new_world = 1;
-                    }
-                }
-                break;
-            case 4:
-                if (num_level == 3)
-                {
-                    if (finBosslevel.viking_mama)
-                    {
-                        new_world = 1;
-                    }
-                }
-                if (num_level == 0x000A)
-                {
-                    if (finBosslevel.space_mama != 0)
-                    {
-                        new_world = 1;
-                    }
-                }
-                break;
-            case 5:
-                if (num_level == 9)
-                {
-                    if (finBosslevel.mr_skops != 0)
-                    {
-                        new_world = 1;
-                    }
-                }
-                break;
-            case 6:
-                if (num_level == 1)
-                {
-                    if (finBosslevel.mr_dark != 0)
-                    {
-                        new_world = 1;
-                    }
-                }
-                break;
-            }
+        case 1:
+            if (num_level == 2 && RayEvts.poing)
+                num_level_choice++;
+
+            if (num_level == 5 && finBosslevel.bzzit)
+                num_level_choice++;
+
+            if (num_level == 7 && RayEvts.hang)
+                new_world = true;
+
+            if (num_level == 13 && finBosslevel.moskito)
+                num_level_choice++;
+
+            if (num_level == 15 && finBosslevel.moskito)
+                new_world = true;
+
+            if (num_level == 16 && RayEvts.grap)
+                new_world = true;
+            break;
+        case 2:
+            if (num_level == 5 && finBosslevel.crazy_drummer)
+                new_world = true;
+
+            if (num_level == 9 && finBosslevel.mr_sax)
+                new_world = true;
+
+            if (num_level == 10 && RayEvts.helico)
+                new_world = true;
+
+            if (num_level == 14 && finBosslevel.mr_sax)
+                new_world = true;
+            break;
+        case 3:
+            if (num_level == 1 && finBosslevel.mr_stone)
+                new_world = true;
+
+            if (num_level == 9 && finBosslevel.mr_stone)
+                new_world = true;
+
+            if (num_level == 10 && RayEvts.run)
+                new_world = true;
+            break;
+        case 4:
+            if (num_level == 3 && finBosslevel.viking_mama)
+                new_world = true;
+
+            if (num_level == 10 && finBosslevel.space_mama)
+                new_world = true;
+            break;
+        case 5:
+            if (num_level == 9 && finBosslevel.mr_skops)
+                new_world = true;
+            break;
+        case 6:
+            if (num_level == 1 && finBosslevel.mr_dark)
+                new_world = true;
+            break;
         }
     }
+
     switch (num_world)
     {
     case 1:
         switch (num_level)
         {
         case 2:
-            if (obj->type == 5)
+            if (obj->type == TYPE_WIZARD1)
             {
-                num_level_choice = 0x0015;
-                var_s0 = 1;
+                num_level_choice = 21;
+                unk_1 = true;
             }
             break;
         case 4:
-            if (obj->type == 5)
+            if (obj->type == TYPE_WIZARD1)
             {
-                num_level_choice = 0x0014;
-                var_s0 = 1;
+                num_level_choice = 20;
+                unk_1 = true;
                 break;
-            }
-            new_world = 1;
-            if (!(t_world_info[1].is_unlocked))
-            {
-                t_world_info[1].is_unlocking = true;
-                t_world_info[18].is_unlocking = true;
-            }
-            break;
-        case 8:
-            new_world = 1;
-            if (!(t_world_info[2].is_unlocked))
-            {
-                t_world_info[2].is_unlocking = true;
-            }
-            if (!(t_world_info[4].is_unlocked))
-            {
-                t_world_info[4].is_unlocking = true;
-            }
-            break;
-        case 11:
-            if (obj->type != 5)
-            {
-                new_world = 1;
-                if (!(t_world_info[3].is_unlocked))
-                {
-                    t_world_info[3].is_unlocking = true;
-                }
             }
             else
             {
-                num_level_choice = 0x0012;
-                var_s0 = 1;
-                break;
+                new_world = true;
+                if (!t_world_info[1].is_unlocked)
+                {
+                    t_world_info[1].is_unlocking = true;
+                    t_world_info[18].is_unlocking = true;
+                }
+            }
+            break;
+        case 8:
+            new_world = true;
+            if (!t_world_info[2].is_unlocked)
+                t_world_info[2].is_unlocking = true;
+
+            if (!t_world_info[4].is_unlocked)
+                t_world_info[4].is_unlocking = true;
+            break;
+        case 11:
+            if (obj->type == TYPE_WIZARD1)
+            {
+                num_level_choice = 18;
+                unk_1 = true;
+            }
+            else
+            {
+                new_world = true;
+                if (!t_world_info[3].is_unlocked)
+                    t_world_info[3].is_unlocking = true;
             }
             break;
         case 12:
-            if (obj->type == 5)
+            if (obj->type == TYPE_WIZARD1)
             {
-                num_level_choice = 0x0013;
-                var_s0 = 1;
-                break;
+                num_level_choice = 19;
+                unk_1 = true;
             }
             break;
         case 17:
-            new_world = 1;
+            new_world = true;
             break;
         case 18:
-            num_level_choice = 0x000B;
-            var_s0 = 1;
+            num_level_choice = 11;
+            unk_1 = true;
             break;
         case 19:
-            num_level_choice = 0x000C;
-            var_s0 = 1;
+            num_level_choice = 12;
+            unk_1 = true;
             break;
         case 20:
             num_level_choice = 4;
-            var_s0 = 1;
+            unk_1 = true;
             break;
-
         case 21:
             num_level_choice = 2;
-            var_s0 = 1;
+            unk_1 = true;
             break;
         }
         break;
@@ -251,42 +167,37 @@ void fix_numlevel(Obj *obj)
         switch (num_level)
         {
         case 4:
-            if (obj->type == 5)
+            if (obj->type == TYPE_WIZARD1)
             {
-                num_level_choice = 0x0011;
-                var_s0 = 1;
-                break;
+                num_level_choice = 17;
+                unk_1 = true;
             }
             break;
         case 6:
-            new_world = 1;
-            if (!(t_world_info[5].is_unlocked))
+            new_world = true;
+            if (!t_world_info[5].is_unlocked)
             {
                 t_world_info[5].is_unlocking = true;
                 t_world_info[19].is_unlocking = true;
             }
             break;
         case 9:
-            if (obj->type == 5)
+            if (obj->type == TYPE_WIZARD1)
             {
-                num_level_choice = 0x0012;
-                var_s0 = 1;
-                break;
+                num_level_choice = 18;
+                unk_1 = true;
             }
             break;
         case 11:
-            new_world = 1;
-            if (!(t_world_info[6].is_unlocked))
-            {
+            new_world = true;
+            if (!t_world_info[6].is_unlocked)
                 t_world_info[6].is_unlocking = true;
-            }
-            if (!(t_world_info[8].is_unlocked))
-            {
+
+            if (!t_world_info[8].is_unlocked)
                 t_world_info[8].is_unlocking = true;
-            }
             break;
         case 13:
-            new_world = 1;
+            new_world = true;
             if (!(t_world_info[7].is_unlocked))
             {
                 t_world_info[7].is_unlocking = true;
@@ -294,15 +205,15 @@ void fix_numlevel(Obj *obj)
             }
             break;
         case 16:
-            new_world = 1;
+            new_world = true;
             break;
         case 17:
             num_level_choice = 4;
-            var_s0 = 1;
+            unk_1 = true;
             break;
         case 18:
             num_level_choice = 9;
-            var_s0 = 1;
+            unk_1 = true;
             break;
         }
         break;
@@ -310,50 +221,44 @@ void fix_numlevel(Obj *obj)
         switch (num_level)
         {
         case 2:
-            new_world = 1;
-            if (!(t_world_info[9].is_unlocked))
-            {
+            new_world = true;
+            if (!t_world_info[9].is_unlocked)
                 t_world_info[9].is_unlocking = true;
-            }
             break;
         case 4:
-            if (obj->type == 5)
+            if (obj->type == TYPE_WIZARD1)
             {
-                num_level_choice = 0x000C;
-                var_s0 = 1;
-                break;
+                num_level_choice = 12;
+                unk_1 = true;
             }
             break;
         case 5:
-            new_world = 1;
-            if (!(t_world_info[10].is_unlocked))
+            new_world = true;
+            if (!t_world_info[10].is_unlocked)
             {
                 t_world_info[10].is_unlocking = true;
                 t_world_info[21].is_unlocking = true;
             }
             break;
         case 9:
-            if (obj->type == 5)
+            if (obj->type == TYPE_WIZARD1)
             {
-                num_level_choice = 0x000D;
-                var_s0 = 1;
-                break;
+                num_level_choice = 13;
+                unk_1 = true;
             }
             break;
         case 11:
-            new_world = 1;
-            if (!(t_world_info[11].is_unlocked))
-            {
+            new_world = true;
+            if (!t_world_info[11].is_unlocked)
                 t_world_info[11].is_unlocking = true;
-            }
             break;
         case 12:
             num_level_choice = 4;
-            var_s0 = 1;
+            unk_1 = true;
             break;
         case 13:
             num_level_choice = 9;
-            var_s0 = 1;
+            unk_1 = true;
             break;
         }
         break;
@@ -361,52 +266,45 @@ void fix_numlevel(Obj *obj)
         switch (num_level)
         {
         case 3:
-            if (obj->type == 5)
+            if (obj->type == TYPE_WIZARD1)
             {
-                num_level_choice = 0x000C;
-                var_s0 = 1;
-                break;
+                num_level_choice = 12;
+                unk_1 = true;
             }
             break;
         case 4:
-            new_world = 1;
-            if (!(t_world_info[12].is_unlocked))
-            {
+            new_world = true;
+            if (!t_world_info[12].is_unlocked)
                 t_world_info[12].is_unlocking = true;
-            }
             break;
         case 7:
-            new_world = 1;
-            if (!(t_world_info[13].is_unlocked))
-            {
+            new_world = true;
+            if (!t_world_info[13].is_unlocked)
                 t_world_info[13].is_unlocking = true;
-            }
             break;
         case 9:
-            if (obj->type == 5)
+            if (obj->type == TYPE_WIZARD1)
             {
-                num_level_choice = 0x000D;
-                var_s0 = 1;
-                break;
+                num_level_choice = 13;
+                unk_1 = true;
             }
             break;
         case 11:
-            new_world = 1;
-            if (!(t_world_info[14].is_unlocked))
+            new_world = true;
+            if (!t_world_info[14].is_unlocked)
             {
                 Vignet_To_Display = 1;
                 t_world_info[14].is_unlocking = true;
-block_132:
                 t_world_info[22].is_unlocking = true;
             }
             break;
         case 12:
             num_level_choice = 3;
-            var_s0 = 1;
+            unk_1 = true;
             break;
         case 13:
             num_level_choice = 9;
-            var_s0 = 1;
+            unk_1 = true;
             break;
         }
         break;
@@ -414,59 +312,59 @@ block_132:
         switch (num_level)
         {
         case 2:
-            if (obj->type == 5)
+            if (obj->type == TYPE_WIZARD1)
             {
-                num_level_choice = 0x000C;
-                var_s0 = 1;
-                break;
+                num_level_choice = 12;
+                unk_1 = true;
             }
-            new_world = 1;
-            if (!(t_world_info[15].is_unlocked))
+            else
             {
-                t_world_info[15].is_unlocking = true;
-                t_world_info[22].is_unlocking = true;
+                new_world = true;
+                if (!t_world_info[15].is_unlocked)
+                {
+                    t_world_info[15].is_unlocking = true;
+                    t_world_info[22].is_unlocking = true;
+                }
             }
             break;
         case 8:
-            new_world = 1;
-            if (!(t_world_info[16].is_unlocked))
+            new_world = true;
+            if (!t_world_info[16].is_unlocked)
             {
                 t_world_info[16].is_unlocking = true;
                 t_world_info[23].is_unlocking = true;
             }
             break;
         case 11:
-            new_world = 1;
+            new_world = true;
             Vignet_To_Display = 1;
             break;
         case 12:
             num_level_choice = 2;
-            var_s0 = 1;
+            unk_1 = true;
             break;
         }
         break;
     case 6:
         if (num_level == 4)
         {
-            new_world = 1;
-            You_Win = 1;
-            fin_du_jeu = 1;
-            fin_dark = 1;
+            new_world = true;
+            You_Win = true;
+            fin_du_jeu = true;
+            fin_dark = true;
         }
         break;
     }
-    if (!(t_world_info[17].is_unlocked) && (Nb_total_cages >= 102))
-    {
+
+    if (!t_world_info[17].is_unlocked && Nb_total_cages >= 102)
         t_world_info[17].is_unlocking = true;
-    }
-    if ((new_world == 0) && (var_s0 == 0))
-    {
+
+    if (!new_world && !unk_1)
         num_level_choice++;
-    }
-    new_level = 1;
+
+    new_level = true;
 }
 const u8 rodata_fix_numlevel[4] = {};
-#endif
 
 /* 36D54 8015B554 -O2 -msoft-float */
 void TEST_SIGNPOST(void)
