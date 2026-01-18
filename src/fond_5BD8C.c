1488e52752637c4e4e50c73bb3a8c1894a00d300
LawlessCowboy <barnabystubbs@live.com>
2026-01-18 16:42:06 +0000

    Added matching code and signature for FUN_80180804 from scratch https://decomp.me/scratch/SyBvZ

--- src/fond_5BD8C.c
+++ src/fond_5BD8C.c
@@ -68,70 +68,56 @@ void DISPLAY_FOND3(void)
 }
 
 /* 5C004 80180804 -O2 -msoft-float */
-#ifndef NONMATCHINGS
-INCLUDE_ASM("asm/nonmatchings/fond_5BD8C", FUN_80180804);
-#else
-/* score of ??? */
 void FUN_80180804(u32 *param_1, s32 param_2, s32 param_3, s16 x, s16 y, s16 h, s16 w)
 {
-    RECT sp10;
+    RECT sp10 = {x, y, w, h};
+    
     RECT sp18;
     RECT sp20;
     RECT sp28;
     RECT sp30;
-
-    sp10.x = x;
-    sp10.y = y;
-    sp10.w = w;
-    sp10.h = h;
-    if (w == 0)
-    {
+    
+    if (w == 0) {
         w = 0x280;
     }
-    if (h == 0)
-    {
+    if (h == 0) {
         h = 0x1e0;
     }
     sp10.x = x + (PS1_CurrentDisplay->drawing_environment).clip.x;
     sp10.y = y + (PS1_CurrentDisplay->drawing_environment).clip.y;
     sp10.w = w;
     sp10.h = h;
-    LoadImage(&sp10, param_1);
+    LoadImage(&sp10,param_1);
     sp18.x = 0;
     sp18.y = (PS1_CurrentDisplay->drawing_environment).clip.y;
     sp18.w = 0x280;
     sp18.h = y;
-    if (y != 0)
-    {
-        ClearImage(&sp18, D_801CF0CA, D_801CF0CA, D_801CF0CA);
+    if (y != 0) {
+        ClearImage(&sp18,D_801CF0CA,D_801CF0CA,D_801CF0CA);
     }
     sp20.x = 0;
     sp20.y = (PS1_CurrentDisplay->drawing_environment).clip.y + y + h;
     sp20.w = 0x280;
     sp20.h = (0x1e0 - y) - h;
-    if (sp20.h != 0)
-    {
-        ClearImage(&sp20, D_801CF0CA, D_801CF0CA, D_801CF0CA);
+    if (sp20.h != 0) {
+        ClearImage(&sp20,D_801CF0CA,D_801CF0CA,D_801CF0CA);
     }
     sp28.x = 0;
     sp28.y = y + (PS1_CurrentDisplay->drawing_environment).clip.y;
     sp28.w = x;
     sp28.h = h;
-    if (x != 0)
-    {
-        ClearImage(&sp28, D_801CF0CA, D_801CF0CA, D_801CF0CA);
+    if (x != 0) {
+        ClearImage(&sp28,D_801CF0CA,D_801CF0CA,D_801CF0CA);
     }
     sp30.x = x + w;
     sp30.y = y + (PS1_CurrentDisplay->drawing_environment).clip.y;
     sp30.w = (0x280 - x) - w;
     sp30.h = h;
-    if (sp30.w != 0)
-    {
-        ClearImage(&sp30, D_801CF0CA, D_801CF0CA, D_801CF0CA);
+    if (sp30.w != 0) {
+        ClearImage(&sp30,D_801CF0CA,D_801CF0CA,D_801CF0CA);
     }
     return;
 }
-#endif
 
 /* 5C1FC 801809FC -O2 -msoft-float */
 void FUN_801809fc(void)
