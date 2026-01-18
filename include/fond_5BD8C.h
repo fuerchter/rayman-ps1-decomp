c89d46579377b1000292c8d89c9264bbd226b3fe
LawlessCowboy <barnabystubbs@live.com>
2026-01-18 13:56:35 +0000

    Added matching code and signature for FUN_8018058c from scratch https://decomp.me/scratch/jSslC

--- include/fond_5BD8C.h
+++ include/fond_5BD8C.h
@@ -4,7 +4,7 @@
 #include "common.h"
 #include "loading_794DC.h"
 
-void FUN_8018058c(u32 *param_1, s32 param_2, s32 param_3, s16 x, s16 y, s16 h, s16 w);
+void FUN_8018058c(u32 *image, s32 param_2, s32 param_3, s16 x, s16 y, s16 h, s16 w);
 void DISPLAY_FOND3(void);
 void FUN_80180804(u32 *param_1, s32 param_2, s32 param_3, s16 x, s16 y, s16 h, s16 w);
 void FUN_801809fc(void);
--- src/fond_5BD8C.c
+++ src/fond_5BD8C.c
@@ -1,72 +1,60 @@
 #include "fond_5BD8C.h"
 
 /* 5BD8C 8018058C -O2 -msoft-float */
-#ifndef NONMATCHINGS
-INCLUDE_ASM("asm/nonmatchings/fond_5BD8C", FUN_8018058c);
-#else
-/*
-score of ???
-strange diff to target. functionally the same though
-*/
-void FUN_8018058c(u32 *param_1, s32 param_2, s32 param_3, s16 x, s16 y, s16 h, s16 w)
+void FUN_8018058c(u32 *image, s32 param_2, s32 param_3, s16 x, s16 y, s16 h, s16 w)
 {
-    RECT sp10;
+    RECT sp10 = {x, y, w, h};
     RECT sp18;
     RECT sp20;
     RECT sp28;
     RECT sp30;
 
-    sp10.x = x;
-    sp10.y = y;
+    if (w == 0) {
+        w = 320;
+    }
+
+    if (h == 0) {
+        h = 240;
+    }
+
+    sp10.x = x + PS1_CurrentDisplay->drawing_environment.clip.x;
+    sp10.y = y + PS1_CurrentDisplay->drawing_environment.clip.y;
     sp10.w = w;
     sp10.h = h;
-    if (w == 0)
-    {
-        w = 0x140;
-    }
-    if (h == 0)
-    {
-        h = 0xf0;
-    }
-    sp10.x = (PS1_CurrentDisplay->drawing_environment).clip.x + x;
-    sp10.y = (PS1_CurrentDisplay->drawing_environment).clip.y + y;
-    sp10.w = w;
-    sp10.h = h;
-    LoadImage(&sp10, param_1);
+    LoadImage(&sp10,image);
+
     sp18.x = 0;
-    sp18.y = (PS1_CurrentDisplay->drawing_environment).clip.y;
-    sp18.w = 0x140;
+    sp18.y = PS1_CurrentDisplay->drawing_environment.clip.y;
+    sp18.w = 320;
     sp18.h = y;
-    if (sp18.h != 0)
-    {
-        ClearImage(&sp18, D_801CF0CA, D_801CF0CA, D_801CF0CA);
+    if (sp18.h != 0) {
+        ClearImage(&sp18,D_801CF0CA,D_801CF0CA,D_801CF0CA);
     }
+
     sp20.x = 0;
-    sp20.y = (PS1_CurrentDisplay->drawing_environment).clip.y + y + h;
-    sp20.w = 0x140;
-    sp20.h = (0xf0 - y) - h;
-    if (sp20.h != 0)
-    {
-        ClearImage(&sp20, D_801CF0CA, D_801CF0CA, D_801CF0CA);
+    sp20.y = y + PS1_CurrentDisplay->drawing_environment.clip.y + h;
+    sp20.w = 320;
+    sp20.h = 240 - y - h;
+    if (sp20.h != 0) {
+        ClearImage(&sp20,D_801CF0CA,D_801CF0CA,D_801CF0CA);
     }
+
     sp28.x = 0;
-    sp28.y = (PS1_CurrentDisplay->drawing_environment).clip.y + y;
+    sp28.y = y + PS1_CurrentDisplay->drawing_environment.clip.y;
     sp28.w = x;
     sp28.h = h;
-    if (sp28.w != 0)
-    {
-        ClearImage(&sp28, D_801CF0CA, D_801CF0CA, D_801CF0CA);
+    if (sp28.w != 0) {
+        ClearImage(&sp28,D_801CF0CA,D_801CF0CA,D_801CF0CA);
     }
+
     sp30.x = x + w;
-    sp30.y = (PS1_CurrentDisplay->drawing_environment).clip.y + y;
-    sp30.w = (0x140 - x) - w;
+    sp30.y = y + PS1_CurrentDisplay->drawing_environment.clip.y;
+    sp30.w = 320 - x - w;
     sp30.h = h;
-    if (sp30.w != 0)
-    {
-        ClearImage(&sp30, D_801CF0CA, D_801CF0CA, D_801CF0CA);
+    if (sp30.w != 0) {
+        ClearImage(&sp30,D_801CF0CA,D_801CF0CA,D_801CF0CA);
     }
 }
-#endif
 
 /* 5BF84 80180784 -O2 -msoft-float */
 void DISPLAY_FOND3(void)
